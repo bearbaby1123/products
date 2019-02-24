1f2611dd694f8efc2fc6f99969aa5d890d8ae820
Jacky Hsiung <forlol1123@gmail.com>
2019-02-24 23:08:10 +0800

    add check file

--- products.py
+++ products.py
@@ -1,11 +1,21 @@
-# Read File
-products = []
-with open('products.csv', 'r', encoding='utf-8') as f:
+import os 
+
+products =[]
+
+#Check File
+if os.path.isfile('products.csv'):
+	print('These is such file')
+	# Read File
+	with open('products.csv', 'r', encoding='utf-8') as f:
 	for line in f:
 		if 'Product,Price' in line:
 			continue
 		name, price = line.strip().split(',')
 		products.append([name, price])
+		print(products)
+else:
+	print('No such file')
+
 # Let User Input
 while True:
 	name  = input('Product: ')
@@ -13,10 +23,12 @@ while True:
 		break
 	price = input('Price:')
 	products.append([name, price])
+
 # Print History
 print(products)
 for p in products:
 	print('I bought a', p[0], 'for', p[1], 'dollars')
+
 # Write File
 with open('products.csv', 'w', encoding='utf-8') as f:
 	f.write('Product,Price\n')
