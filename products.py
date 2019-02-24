b0ee0ad2ae5eda1d99b028e62ecd12559d02e447
Jacky Hsiung <forlol1123@gmail.com>
2019-02-24 20:28:53 +0800

    add write csv

--- products.py
+++ products.py
@@ -10,4 +10,9 @@ print(products)
 
 for p in products:
 	print('I bought a', p[0], 'for', p[1], 'dollars')
-	
+
+with open('products.csv', 'w', encoding='utf-8') as f:
+	f.write('Product,Price\n')
+	for p in products:
+		f.write(p[0] + ',' + p[1] + '\n')
+
